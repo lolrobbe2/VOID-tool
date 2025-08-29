fa88c009b3dc35a2a5ad4c0fafd58fc86ad13cc8
Beernaert Robbe <robbebeernaert@hotmail.be>
2025-08-29 02:07:57 +0200

    added the proper parent class

--- foxhole-void-bot/src/commands/StockpilesModule.cs
+++ foxhole-void-bot/src/commands/StockpilesModule.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 [SlashCommand("stockpiles", "Stockpiles command")]
-public class StockpileCommands
+public class StockpileCommands : ApplicationCommandModule<ApplicationCommandContext>
 {
     private readonly StockpilesRepository _repository;
 
