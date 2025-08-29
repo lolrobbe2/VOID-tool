b68567afe7929fe6cfd0fae3792ba16bf60d09c0
Beernaert Robbe <robbebeernaert@hotmail.be>
2025-08-29 02:01:40 +0200

    added stockpile module

---
+++ foxhole-void-bot/src/commands/StockpilesModule.cs
@@ -0,0 +1,33 @@
+using FoxholeBot.repositories;
+using NetCord.Services.ApplicationCommands;
+using System.Text;
+using System.Threading.Tasks;
+
+[SlashCommand("stockpiles", "Stockpiles command")]
+public class StockpileCommands
+{
+    private readonly StockpilesRepository _repository;
+
+    public StockpileCommands(StockpilesRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [SubSlashCommand("list", "Get stockpiles")]
+    public string ListStockpiles(SlashCommandContext context)
+    {
+        var stockpiles = _repository.GetAllStockpilesAsync().GetAwaiter().GetResult();
+
+        if (stockpiles == null || stockpiles.Length == 0)
+            return "No stockpiles found.";
+
+        var message = new StringBuilder();
+        foreach (var sp in stockpiles)
+        {
+            message.AppendLine($"**{sp.Name}** — `{sp.Code}` ({sp.Region}/{sp.Subregion})");
+        }
+
+        return message.ToString();
+    }
+
+}
