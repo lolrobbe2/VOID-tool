e62ac96eea9cde4ca0908cc3ccb7e94c55f53877
Beernaert Robbe <robbebeernaert@hotmail.be>
2025-09-01 18:20:48 +0200

    added videos and razor pages

---
+++ foxhole-void-bot/src/Components/VideoCarousel/VideoCarouselViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+public class VideoCarouselViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var videoUrls = new List<string>
+        {
+            "https://www.youtube.com/embed/i-CYRlveLnU",
+            "https://www.youtube.com/embed/HHLzJd2FOQQ",
+            "https://www.youtube.com/embed/abdD3kgB4cg"
+            // Add more URLs as needed
+        };
+
+        return View(videoUrls);
+    }
+}
