84d6c9de671a6920d4b1cf3ae0f0b329d2f688b5
Beernaert Robbe <robbebeernaert@hotmail.be>
2025-09-16 12:33:06 +0200

    added Coordsvector and neccesary calc functions

---
+++ foxhole-void-bot/src/types/ArtyGun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FoxholeBot.types
+{
+    public class ArtyGun
+    {
+        //artillery bias per level
+        public byte Bias;
+        //name of the artillery gun
+        public string Name;
+        //max range of the artillery gun
+        public UInt16 MaxRange;
+        //min range of the artillery gun
+        public UInt16 MinRange;
+    }
+}
---
+++ foxhole-void-bot/src/types/CoordsVector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FoxholeBot.types
+{
+    public struct Coordinate
+    {
+        public UInt16 X;
+        public UInt16 Y;
+    }
+    public class CoordsVector
+    {
+        Coordinate start;
+        Coordinate end;
+
+        /// <summary>
+        /// This function calculates the vertical distance of the two coordintates
+        /// </summary>
+        /// <returns></returns>
+        public UInt16 GetVerticalDistance()
+        {
+            return (UInt16)(end.Y - start.Y);
+        }
+
+        public UInt16 GetHorizontalDistance()
+        {
+            return (UInt16)(end.X - end.Y);
+        }
+
+        /// <summary>
+        /// This function calculates the distance between the two coordinates
+        /// </summary>
+        /// <returns>UInt16 with the hypothenuse distance</returns>
+        public UInt16 GetVectorDistance()
+        {
+            double VDistanceSquared = Math.Pow(GetVerticalDistance(), 2);
+            double HDistanceSquared = Math.Pow(GetHorizontalDistance(),2);
+            return (UInt16)Math.Sqrt(VDistanceSquared + HDistanceSquared);
+        }
+
+        /// <summary>
+        /// This function calculates the angle between the vertical axis and the hypothenuse with 0 degrees at the top
+        /// </summary>
+        /// <returns></returns>
+        public float GetAngle()
+        {
+            //we do 360 minus as foxhole 0° is at the top.
+            return 360 - (float)Math.Asin((double)GetHorizontalDistance() / (double)GetVectorDistance());
+        }
+    }
+}
