675a1c6f4228bcfe2c2e545807d32438cf9b5fa7
Beernaert Robbe <robbebeernaert@hotmail.be>
2025-11-23 14:05:22 +0100

    feat: DiscordSDK

--- foxhole-void-bot/src/Discord/DiscordSdk.cs
+++ foxhole-void-bot/src/Discord/DiscordSdk.cs
@@ -13,40 +13,50 @@ namespace FoxholeBot.src.Discord
 {
     public class DiscordSDK
     {
+        private static DiscordSDK? SingletonSDK { get; set; }
+
         private HttpClient client { get; set; }
         private NavigationManager navigation { get; set; }
         private IJSRuntime js { get; set; }
         private DiscordActivityBridge bridge { get; set; }
 
-        private IDictionary<string, Task> pendingCommands;
+        IDictionary<string, TaskCompletionSource<object>> pendingCommands = new Dictionary<string, TaskCompletionSource<object>>();
         public DiscordSDK(IJSRuntime js, HttpClient client, NavigationManager navigation)
         {
             this.client = client;
             this.js = js;
             this.navigation = navigation;
             this.bridge = new DiscordActivityBridge(js);
+            SingletonSDK = this;
+        }
+        public void OnMessage(object data)
+        {
+            var taskSource = pendingCommands["nonce here"];
         }
         [JSInvokable]
         public static void ReceiveDiscordMessage(object data)
         {
             // data is the message received via postMessage
             Console.WriteLine("Discord message received:");
-            Console.WriteLine(data);
-
+            SingletonSDK.OnMessage(data);
         }
-        Task SendCommand(object payload)
-        {
-            string nonce =  Guid.NewGuid().ToString();  
-            pendingCommands.Add(nonce,bridge.PostCommandAsync(Opcodes.FRAME, payload, nonce));
 
-            if (pendingCommands.TryGetValue(nonce, out Task command))
-            {
-                return command;
-            } 
-            else 
-            {
-                return Task.CompletedTask;
-            }
+        /// <summary>
+        /// this function sends a commands to the bridge and then ackowladges it
+        /// </summary>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        async Task<object> SendCommandAsync(object payload)
+        {
+            string nonce = Guid.NewGuid().ToString();
+
+            // PostCommandAsync should return Task<T>
+            var taskSource = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            await bridge.PostCommandAsync(Opcodes.FRAME, payload, nonce);
+
+            pendingCommands.Add(nonce, taskSource);
+            return await taskSource.Task;
         }
     }
 }
